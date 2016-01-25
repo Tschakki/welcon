51426e1b11e2b3e6f8cd911d44b10dbf23d9b614
animina <animina@riseup.net>
2016-01-26 00:44:23 +0100

    padding of pic changed

--- entry.php
+++ entry.php
@@ -1,10 +1,13 @@
 <?php include_once("header.php") ?>
+
+
+
 <div id="" class="row">
-    <div id="pic" class="col-md-6"></div>
+    <div id="pic" class="col-md-6" style="padding-top:1em" ></div>
     <div id="marker" class="col-md-6"><div id="map"></div></div>
 </div>
 <div class="row">
-    <div id="message" class="col-md-6"></div>
     <div id="details" class="col-md-6"></div>
+    <!--<div id="details" class="col-md-6"></div>-->
 </div>
 <?php include_once("footer.php") ?>
