69a248e16ea9bb2150effc8975db41fb35504197
animina <animina@riseup.net>
2016-01-26 00:51:10 +0100

    [entry.php] details left of pic

--- entry.php
+++ entry.php
@@ -3,11 +3,21 @@
 
 
 <div id="" class="row">
-    <div id="pic" class="col-md-6" style="padding-top:1em" ></div>
-    <div id="marker" class="col-md-6"><div id="map"></div></div>
+    <div id="details" class="col-md-1" style="padding-top:1em" ></div>
+    <div id="pic" class="col-md-4" style="padding-top:1em" ></div>
+   
+    <div id="marker" class="col-md-7"><div id="map"></div></div>
+    <!--<div id="" class="col-md-2" style="padding-top:1em" ></div>
+     <div id="" class="col-md-1" style="padding-top:1em" ></div>-->
 </div>
-<div class="row">
+<!--<div class="row">
+    <div id="" class="col-md-1" style="padding-top:1em" ></div>
+    <div id="pic" class="col-md-4" style="padding-top:1em" ></div>
+   
+    <div id="marker" class="col-md-4"><div id="map"></div></div>
+    <div id="" class="col-md-2" style="padding-top:1em" ></div>
+     <div id="" class="col-md-1" style="padding-top:1em" ></div>
     <div id="details" class="col-md-6"></div>
-    <!--<div id="details" class="col-md-6"></div>-->
-</div>
+    <div id="details" class="col-md-6"></div>
+</div>-->
 <?php include_once("footer.php") ?>
