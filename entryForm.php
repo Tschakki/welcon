5f37cc6667777213e0691f036ec4666b27328ba9
tschakki <tschakki@posteo.de>
2016-01-26 00:28:46 +0100

    [entryForm.php] changes classes ob elements to bootstrap-style

--- entryForm.php
+++ entryForm.php
@@ -1,20 +1,20 @@
 <?php include_once("header.php") ?>
 <section id="submitForm">
     <br>
-<h1Body>what do you announce?</h1Body>
+<h2>what do you announce?</h2>
     <br>
     
-    <form id="editNeed" class="form" method="post" novalidate="novalidate">
-    <p>
+    <form id="editNeed" method="post" role="form" novalidate="novalidate">
+    <div class="form-group">
     <label class="frontLable" for="chooseKind">Do you offer or need?:</label>
-    <select name="chooseKind" id="chooseKind" class="form" form="editNeed" required autofocus>
+    <select name="chooseKind" id="chooseKind" form="editNeed" class="form-control" required autofocus>
         <option value="offer">Offer</option>
         <option value="need">Need</option>
     </select>
-    </p>
-    <p>
+    </div>
+    <div class="form-group">
     <label class="frontLable" for="chooseCategory">Choose:</label>
-    <select name="chooseCategory" id="chooseCategory" form="editNeed" class="form" required autofocus>
+    <select name="chooseCategory" id="chooseCategory" form="editNeed" class="form-control" required autofocus>
         <option value="cloth">cloth</option>
         <option value="rooms">rooms</option>
         <option value="food">food</option>
@@ -24,36 +24,35 @@
         <option value="contact">contact</option>
         <option value="other">something else</option>
     </select>
-    </p>
-    <p>
-    <label class="frontLable" for="title">Title:</label>
-    <input id="title" name="title" class="form" form="editNeed" size="25" ></input>
-    </p>
-<p>
-    <label class="frontLable" for="description">Description:</label>
-    <textarea name="description" id="description" form="editNeed" class="form" cols="75" rows="15"></textarea>
-    </p>
-    <p> 
-        
+    </div>
+    <div class="form-group">
+        <label class="frontLable" for="title">Title:</label>
+        <input id="title" name="title" form="editNeed" size="25" class="form-control" />
+    </div>
+    <div class="form-group">
+        <label class="frontLable" for="description">Description:</label>
+        <textarea name="description" id="description" form="editNeed" cols="75" rows="15" class="form-control"></textarea>
+    </div>
+    <div class="form-group row">
         <label class="frontLable" for="latitude">Location:</label>
-        
-    <section id="map"></section>
-<input id="latitude" type="hidden" name="latitude" form="editNeed" class="form" placeholder="Latitude"  />
-    <input id="longitude" type="hidden" name="longitude" form="editNeed" class="form" placeholder="Longitude"  />
-    
-    </p>
-    <p>
-    <label class="frontLable" for="name">Author:</label>
-    <input type="name" name="name" id="name" form="editNeed" class="form" size="25" ></input>
-    </p>
-    <p>
-    <label class="frontLable" for="image"></label>
-    <input type="hidden" name="image" id="image" form="editNeed" class="form"></input>
-    </p>
-    <p>
-    <label class="frontLable" for="email">Contact Email:</label>
-    <input type="email" name="email" id="email" form="editNeed" class="form" size="25" ></input>
-    </p>
+    </div>
+    <div class="form-group row">
+            <section id="map"></section>
+            <input id="latitude" type="hidden" name="latitude" form="editNeed" placeholder="Latitude" class="form-control"  />
+            <input id="longitude" type="hidden" name="longitude" form="editNeed" placeholder="Longitude" class="form-control"  />  
+    </div>
+    <div class="form-group">
+        <label class="frontLable" for="name">Author:</label>
+        <input type="name" name="name" id="name" form="editNeed" size="25" class="form-control" />
+    </div>
+    <div class="form-group">
+        <label class="frontLable" for="image"></label>
+        <input type="hidden" name="image" id="image" form="editNeed" class="form-control" />
+    </div>
+    <div class="form-group">
+        <label class="frontLable" for="email">Contact Email:</label>
+        <input type="email" name="email" id="email" form="editNeed" size="25" class="form-control" />
+    </div>
 </form>
 <form id="imageUpload" class="form" method="post" action="upload.php"></form>
     
@@ -72,11 +71,12 @@
         </div>
     
     
-    <p><label class="frontLable" for="submitbtn"></label>
-        <button type="submit" class="form" id="submitbtn" form="editNeed">
+    <div class="form-group">
+        <label class="frontLable" for="submitbtn"></label>
+        <button type="submit" class="btn-primary" id="submitbtn" form="editNeed">
         <img src="res/save8.png" class="save"/>
         </button>
-    </p>
+    </div>
 </div>
 </section>
 <?php include_once("footer.php") ?>
